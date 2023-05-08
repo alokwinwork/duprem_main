38076f3158268fc52c69ef4e3a941d8a4374e44f
alokwinwork <95196583+alokwinwork@users.noreply.github.com>
2023-05-08 05:57:32 +0530

    Update app.py

--- duprem_main/app.py
+++ duprem_main/app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+####import json_to_csv
+####import remove_duplicates
+####import csv_to_json
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return 'Hello World'
+@app.route('/', methods=['POST'])
+def process_json_input():
+    # Get the JSON input data from the request body
+    json_input = request.json
+
+    # Validate the JSON input
+    # Assumes that the validation logic is implemented in a separate microservice
+    # and can be accessed via the 'validate-service' endpoint
+    validate_service_url = "http://validate-service:5001/"
+    response = requests.post(validate_service_url, json=json_input)
+    if not response.ok:
+        return jsonify(error="Invalid JSON input"), 400
+
+    # Convert the JSON input to CSV
+    ####csv_data = json_to_csv.convert(json_input)
+
+    # Remove duplicates from the CSV data
+    ####deduplicated_csv_data = remove_duplicates.remove_duplicates(csv_data)
+
+    # Convert the deduplicated CSV data back to JSON
+    ####json_output = csv_to_json.convert(deduplicated_csv_data)
+
+    # Return the JSON output
+    ####return jsonify(json_output), 200
 
 if __name__ == '__main__':
-   app.run()
+    app.run(debug=True, host='0.0.0.0')
